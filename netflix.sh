f6e1fa305b3b9cf8d37755a596014eba95414f4e
justinweichTV <64624657+justinweichTV@users.noreply.github.com>
2020-09-24 22:08:46 +0200

    netflix-launscher.sh
    please install it with the installer!

---
+++ netflix.sh
@@ -0,0 +1,8 @@
+echo Welcome to our netflix launscher!
+echo you need the Firefox to use tihs launscher!
+eche use sudo apt-get install firefox
+echo trying to start Netflix ....
+echo.
+echo.
+echo We dont save your data! and we are not netflix!
+firefox https://netflix.com
